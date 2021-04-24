f3af03be5f8ee50a43ab4c372f20ddcb2f93774c
KotiDhana <dhanakotiroxx@gmail.com>
2021-04-24 22:51:52 +0530

    Hello World

--- hello_world/hello_world.go
+++ hello_world/hello_world.go
@@ -1,9 +1,13 @@
+// Go files must have a package declaration as the first line of source code
+// NB: The `main` package is a special name that's used to build an executable.
 package main
 
+// 'import' statement is used to define which packages are required for this program
 import (
 	"fmt"
 )
 
+//systax to define function. 'main' function indicates the start of the program
 func main() {
 	fmt.Println("Hello, World!")
 }
